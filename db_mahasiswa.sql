0cb9613577dc9c5b4f28cd272245e05f69a041e1
dedealamsyah <dedealamsyah29@gmail.com>
2025-10-04 07:20:48 +0700

    modified:   db_mahasiswa.sql

--- db_mahasiswa.sql
+++ db_mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Okt 2025 pada 17.24
+-- Waktu pembuatan: 04 Okt 2025 pada 02.20
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.0.30
 
